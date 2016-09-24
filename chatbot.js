b751480635e97e1430b7b992eb072ca2c4715e0b
Matthew <matthewbrittain3d@gmail.com>
2016-09-23 20:02:13 -0700

    Finished Majority Of Project
    Almost done, sorry for not commiting in a long time.

--- chatbot.js
+++ chatbot.js
@@ -1,3 +1,40 @@
-function chatFunction() {
-  document.getElementById("chat-area").textContent = document.getElementById("input").value;
+function talkButton() {
+  var objDiv = document.getElementById("chat-area");
+  var greetResponse = ["Hello", "Hey", "How's it going?", "Hi", "Greetings", "Howdy"]
+  var randomResponse = Math.floor(Math.random() * 6) + 0;
+  var currentdate = new Date();
+  var hours = currentdate.getHours();
+  if (hours > 12) {
+    hours -= 12;
+  }
+  else if (hours === 0) {
+     hours = 12;
+  }
+  var datetime = "It is " + hours + ":" + currentdate.getMinutes();
+  var response = {
+    "what is your name": "Alleria",
+    "hey": greetResponse[randomResponse],
+    "hello": greetResponse[randomResponse],
+    "sup": greetResponse[randomResponse],
+    "what time is it": datetime,
+  }
+
+  var nonsense = ["Huh?","I didn't quite understand.","What?"]
+  var randomResponse2 = Math.floor(Math.random() * 3) + 0;
+  if(document.getElementById('input').value != "") {
+  var message = document.getElementById('input').value;
+  message = message.toLowerCase();
+  $("#chat-area").append(document.getElementById('input').value += "</br>");
+  if (response[message] === undefined || response[message] === null) {
+     $("#chat-area").append("<div class='grey'>" + nonsense[random] + "</div></br>");
+     document.getElementById('input').value = "";
+     objDiv.scrollTop = objDiv.scrollHeight;
+}
+else {
+  $("#chat-area").append("<div class='grey'>" + response[message] + "</div>" + "</br>");
+  console.log(response[message]);
+  document.getElementById('input').value = "";
+  objDiv.scrollTop = objDiv.scrollHeight;
+}
+}
 }
